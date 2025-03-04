adaf3e7b939654e495a1f0e1f1185df390a1c20a
TheDumbTechGuy <sfroelich01@gmail.com>
2025-03-04 20:41:37 +0000

    use configured foreign key and primary key types

--- lib/generators/active_shrine/install/templates/db/migrate/create_active_shrine_attachments.rb
+++ lib/generators/active_shrine/install/templates/db/migrate/create_active_shrine_attachments.rb
@@ -2,8 +2,11 @@
 
 class CreateActiveShrineAttachments < ActiveRecord::Migration[7.0]
   def change
-    create_table :active_shrine_attachments do |t|
-      t.belongs_to :record, polymorphic: true, null: true
+    # Use Active Record's configured type for primary and foreign keys
+    primary_key_type, foreign_key_type = primary_and_foreign_key_types
+
+    create_table :active_shrine_attachments, id: primary_key_type do |t|
+      t.belongs_to :record, polymorphic: true, null: true, type: foreign_key_type
       t.string :name, null: false
       t.string :type, null: false, default: "ActiveShrine::Attachment"
       if ActiveRecord::Base.connection.adapter_name.downcase.include?("postgresql")
@@ -16,6 +19,7 @@ class CreateActiveShrineAttachments < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
+    
     add_index :active_shrine_attachments, :name
     if ActiveRecord::Base.connection.adapter_name.downcase.include?("postgresql")
       add_index :active_shrine_attachments, :file_data, using: :gin
@@ -24,5 +28,15 @@ class CreateActiveShrineAttachments < ActiveRecord::Migration[7.0]
       add_index :active_shrine_attachments, :file_data
       add_index :active_shrine_attachments, :metadata
     end
+  end
+
+  private
+  
+  def primary_and_foreign_key_types
+    config = Rails.configuration.generators
+    setting = config.options[config.orm][:primary_key_type]
+    primary_key_type = setting || :primary_key
+    foreign_key_type = setting || :bigint
+    [ primary_key_type, foreign_key_type ]
   end
 end
--- spec/internal/db/migrate/20231212163402_create_active_shrine_attachments.rb
+++ spec/internal/db/migrate/20231212163402_create_active_shrine_attachments.rb
@@ -2,8 +2,11 @@
 
 class CreateActiveShrineAttachments < ActiveRecord::Migration[7.0]
   def change
-    create_table :shrine_attachments do |t|
-      t.belongs_to :record, polymorphic: true, null: true
+    # Use Active Record's configured type for primary and foreign keys
+    primary_key_type, foreign_key_type = primary_and_foreign_key_types
+
+    create_table :shrine_attachments, id: primary_key_type do |t|
+      t.belongs_to :record, polymorphic: true, null: true, type: foreign_key_type
       t.string :name, null: false
       t.string :type, null: false, default: "ActiveShrine::Attachment"
       if ActiveRecord::Base.connection.adapter_name.downcase.include?("postgresql")
@@ -19,5 +22,15 @@ class CreateActiveShrineAttachments < ActiveRecord::Migration[7.0]
     add_index :shrine_attachments, :name
     add_index :shrine_attachments, :file_data, using: :gin
     add_index :shrine_attachments, :metadata, using: :gin
+  end
+
+  private
+  
+  def primary_and_foreign_key_types
+    config = Rails.configuration.generators
+    setting = config.options[config.orm][:primary_key_type]
+    primary_key_type = setting || :primary_key
+    foreign_key_type = setting || :bigint
+    [ primary_key_type, foreign_key_type ]
   end
 end
